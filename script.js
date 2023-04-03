779e9136d2138a8b271087cc88d18d72e590ed7b
rweventos <rweventos.rj@gmail.com>
2023-04-03 00:00:17 -0300

    Added three page html and addes some stylish to the pages.

--- script.js
+++ script.js
@@ -13,3 +13,43 @@ if (close) {
         nav.classList.remove('activeNav');
     })
 }
+
+//Section for single product page to change image 
+
+var mainImg = document.getElementById('mainImage');
+var smallImg= document.getElementsByClassName('small-img');
+var path = 'assets/images/product/f';
+smallImg[0].onclick = () => {
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = () => {
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = () => {
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = () => {
+    mainImg.src = smallImg[3].src;
+}
+
+function incrementValue() {
+    var value = parseInt(document.getElementById('quantidade').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value < 100) {
+      value++;
+      document.getElementById('quantidade').value = value;
+    }
+  }
+
+  function decrementValue() {
+    var value = parseInt(document.getElementById('quantidade').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value > 0) {
+      value--;
+      document.getElementById('quantidade').value = value;
+    }
+  }
+
+
+
+
