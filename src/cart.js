cb92e1babf2143725ffd5da30cf1609097b53827
rweventos <rweventos.rj@gmail.com>
2023-04-08 09:04:33 -0300

    some modified files

--- src/cart.js
+++ src/cart.js
@@ -1,1 +1,38 @@
 let basket = JSON.parse(localStorage.getItem("data")) || [];
+let table = document.getElementById("table");
+
+let calculation = () => {
+   let cartIcon = document.getElementById("cartAmount");
+   let mobileCartIcon = document.getElementById("mobileCartAmount");
+   cartIcon.innerHTML = basket
+      .map((x) => x.quantidade)
+      .reduce((sum, item) => sum + item, 0);
+   mobileCartIcon.innerHTML = basket
+      .map((x) => x.quantidade)
+      .reduce((sum, item) => sum + item, 0);
+};
+
+function showBadge() {
+   let cartIcon = document.getElementById("cartAmount");
+   let mobileCartIcon = document.getElementById("mobileCartAmount");
+
+   if (cartIcon.innerHTML === "0") {
+      cartIcon.classList.remove("shown");
+   } else {
+      cartIcon.classList.add("shown");
+   }
+   if (mobileCartIcon.innerHTML === "0") {
+      mobileCartIcon.classList.remove("shown");
+   } else {
+      mobileCartIcon.classList.add("shown");
+   }
+}
+let stateCheck = setInterval(() => {
+   if (document.readyState === "complete") {
+      clearInterval(stateCheck);
+      showBadge();
+   }
+}, 500);
+
+calculation();
+showBadge();
